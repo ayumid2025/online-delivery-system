6fd3c4515af6c76d65235fecf001f46e84438941
ayumid2025 <bible9683@gmail.com>
2026-03-27 10:50:39 +0300

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Checkout from './components/Checkout';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/checkout/:orderId" element={<Checkout />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
